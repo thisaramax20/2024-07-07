32ad6959e212812a99b03e0079ae249e5e1f354b
thisaramax20 <thisarathejana20@gmail.com>
2024-07-07 14:05:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+
+
+function fillAll(){
+
+    let tblCountries = document.getElementById("tblCountries");
+
+let tblBody = `<tr>
+            <th>Flag</th>
+            <th>Name</th>
+            <th>Capital</th>
+            <th>Population</th>
+        </tr>`;
+
+    fetch("https://restcountries.com/v3.1/all").then((res)=>res.json()).then(data=>{
+        data.forEach(element => {
+            tblBody += `<tr>
+                            <td>${element.flag}</td>
+                            <td>${element.name.common}</td>
+                            <td>${element.capital}</td>
+                            <td>${element.population}</td>
+                        </tr>`
+            
+            tblCountries.innerHTML=tblBody;            
+        });
+    })
+}        
+
+
+
+function searchData(){
+
+    let userInput = document.getElementById("searchItem").value;
+    console.log(userInput);
+
+    let name = document.getElementById("name");
+    let capital = document.getElementById("capital");
+    let population = document.getElementById("population");
+    let flag = document.getElementById("flag");
+
+    fetch(`https://restcountries.com/v3.1/name/${userInput}`).then(res=>res.json()).then(data=>{
+        data.forEach(element=>{
+            name.innerText = element.name.common;
+            capital.innerText = element.capital;
+            population.innerText = element.population;
+            flag.src = element.flags.png;
+        })
+    })
+}
